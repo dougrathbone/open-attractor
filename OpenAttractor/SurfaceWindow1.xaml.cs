c5da3d1b9285f90846dff6a3f8ecaa83f7d601c9
Doug Rathbone <dougrathbone@gmail.com>
2012-05-29 18:27:59 +1000

    worked on video player

--- OpenAttractor/SurfaceWindow1.xaml.cs
+++ OpenAttractor/SurfaceWindow1.xaml.cs
@@ -99,5 +99,47 @@ namespace OpenAttractor
         {
             //TODO: disable audio, animations here
         }
+
+		private void SurfaceButton_Click(object sender, RoutedEventArgs e)
+		{
+			videoPlayer.MediaEnded += delegate(object o, RoutedEventArgs args)
+			                          	{
+			                          		videoPlayer.Position = new TimeSpan(0,0,0,0);
+											videoPlayer.Play();
+			                          	};
+			videoPlayer.Play();
+			PlayButton.Visibility = Visibility.Hidden;
+			Panel.SetZIndex(PlayButton,-1);
+
+			PauseButton.Visibility = Visibility.Visible;
+			RewindButton.Visibility = Visibility.Visible;
+		}
+
+		private void RewindButton_Click(object sender, RoutedEventArgs e)
+		{
+			videoPlayer.Position = new TimeSpan(0,0,0,0);
+		}
+
+		private void PauseButton_Click(object sender, RoutedEventArgs e)
+		{
+			videoPlayer.Pause();
+
+			((SurfaceButton)sender).Visibility = Visibility.Collapsed;
+			PlayButtonSmall.Visibility = Visibility.Visible;
+		}
+
+		private void videoPlayer_Loaded(object sender, RoutedEventArgs e)
+		{
+			((MediaElement)sender).Play();
+			((MediaElement)sender).Pause();
+		}
+
+		private void PlayButtonSmall_Click(object sender, RoutedEventArgs e)
+		{
+			videoPlayer.Play();
+
+			((SurfaceButton)sender).Visibility = Visibility.Collapsed;
+			PauseButton.Visibility = Visibility.Visible;
+		}
     }
 }
